c39798e01790b78458889eb5f6498acab89d73f5
Heather Kirk <hskirk@gmail.com>
2019-05-02 23:54:51 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,37 @@
 class TweetsController < ApplicationController
-
-
-end
+  
+    get '/tweets/new' do
+      @users = User.all
+      erb :'/tweets/new' 
+    end 
+  
+    post '/tweets' do
+     @tweet = Tweet.create(params[:tweet])
+      @tweet.save 
+      redirect "tweets/#{@tweet_id}"
+    end 
+  
+    get '/tweets/:id' do
+      @tweet = Tweet.find(params[:id])
+      erb :'/tweets/show'
+    end
+    
+    get '/tweets/:id/edit' do
+      @tweet = Tweet.find(params[:id])
+      @users = User.all
+      erb :'/tweets/edit'
+    end 
+    
+    patch '/tweets/:id' do
+      @tweet = Tweet.find(params[:id])
+      @tweet.update(params["tweet"])
+      @tweet.save
+      redirect "tweets/#{@tweet_id}"
+    end 
+    
+    delete '/tweets/:id/delete' do
+      @tweet = Tweet.find(params[:id])
+      @tweet.delete
+      redirect '/tweets'
+    end 
+  end  
